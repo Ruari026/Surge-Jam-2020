cb241a35b24af68dd2baa74eb99dc51dd6d658b6
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-10-03 13:02:47 +0100

    Minor Hierarchy Fixes

--- Assets/Scripts/Audience/States/AudienceState.cs
+++ Assets/Scripts/Audience/States/AudienceState.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AudienceState : MonoBehaviour
+public class AudienceState
 {
     public virtual void StartState(AudienceMemberController theAudienceMember)
     {
