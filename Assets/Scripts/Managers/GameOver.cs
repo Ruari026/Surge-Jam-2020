40ac6e6c1781483234b67d2955915430b5369c68
Kylejohn175 <kylejohn175@hotmail.co.uk>
2020-10-03 16:30:46 +0100

    Updated Game Over Screen

--- Assets/Scripts/Managers/GameOver.cs
+++ Assets/Scripts/Managers/GameOver.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class GameOver : MonoBehaviour
 {
-   public void PlayGame()
+    public void ReturnToMenu()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
-
 }
