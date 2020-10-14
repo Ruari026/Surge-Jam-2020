db27af326d54eb29f9d56cefcc24ff63a87958b6
Kyle <kylejohn175@gmail.com>
2020-10-14 20:55:03 +0100

    Updated Main Menu

---
+++ Assets/Scripts/URL/WebsiteLink.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebsiteLink : MonoBehaviour
+{
+    public void MarbleLink()
+    {
+        Application.OpenURL("http://www.unbosi.org/marbles/");
+    }
+
+    public void CollabLink()
+    {
+        Application.OpenURL("https://appdeveloperscotland.co.uk/");
+    }
+}
