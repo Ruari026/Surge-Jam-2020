00a00491be4001c5722e6e0afa8ae0a6dea538e1
Bmckel201 <45988940+Bmckel201@users.noreply.github.com>
2020-10-20 20:56:28 +0100

    button

---
+++ Assets/Scripts/URL/TeamLinks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamLinks : MonoBehaviour
+{
+    public void RuariLink()
+    {
+        Application.OpenURL("https://twitter.com/Ruari026");
+    }
+
+    public void BrunoLink()
+    {
+        Application.OpenURL("https://twitter.com/BPa_sho");
+    }
+
+    public void MalwinaLink()
+    {
+        Application.OpenURL("https://malinajab.carbonmade.com/");
+    }
+
+    public void KyleLink()
+    {
+        Application.OpenURL("https://kylegamesdesign.wixsite.com/kylegamedesign");
+    }
+
+    public void BrionyLink()
+    {
+        Application.OpenURL("https://twitter.com/briony_mckelvie");
+    }
+}
