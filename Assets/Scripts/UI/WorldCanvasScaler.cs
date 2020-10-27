51fbc00c223ebf71a85b3f329c0b211def3c23dd
Ruari026 <36458213+Ruari026@users.noreply.github.com>
2020-10-27 15:27:23 +0000

    Removing Multiple Cameras

---
+++ Assets/Scripts/UI/WorldCanvasScaler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCanvasScaler : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 referenceResolution;
+
+    private void Start()
+    {
+
+        Vector2 newScale = new Vector2
+        {
+            x = Screen.width / referenceResolution.x,
+            y = Screen.height / referenceResolution.y
+        };
+
+        RectTransform rect = this.GetComponent<RectTransform>();
+        Vector2 newSize = rect.sizeDelta;
+        newSize.x *= newScale.x;
+        newSize.y *= newScale.y;
+        rect.sizeDelta = newSize;
+    }
+}
